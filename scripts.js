b1cde34418dda317847bb20b290ed8646d2fe37c
Theo Björkqvist <theobjorkqvist@gmail.com>
2025-09-24 18:37:03 +0200

    pasterar in struktur för uppgift 3

--- scripts.js
+++ scripts.js
@@ -1,27 +1,52 @@
-const secondHand=document.querySelector('.sec-hand');
-const minhand=document.querySelector('.min-hand');
-const hourHand=document.querySelector('.hour-hand');
-function setDate(){
-    const now =new Date();
-    const seconds= now.getSeconds();
-    const secondsDegrees=((seconds/60)*360)+90;
-    secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
+//uppgift 1
+const secondHand = document.querySelector('.sec-hand');
+const minhand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins=now.getMinutes();
-    const minDegrees=((mins/60)*360)+90;
-    minhand.style.transform=`rotate(${minDegrees}deg)`;
+  const mins = now.getMinutes();
+  const minDegrees = ((mins / 60) * 360) + 90;
+  minhand.style.transform = `rotate(${minDegrees}deg)`;
 
-    const hours=now.getHours();
-    const hourDegrees=((hours/12)*360)+90;
-    hourHand.style.transform=`rotate(${hourDegrees}deg)`;
+  const hours = now.getHours();
+  const hourDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
 }
-setInterval(setDate,1000);
+setInterval(setDate, 1000);
+//uppgift2
+const inputs = document.querySelectorAll('.controls input');
 
-const inputs=document.querySelectorAll('.controls input');
-
-function handleUpdate(){
-    const suffix=this.dataset.sizing || "";
-    document.documentElement.style.setProperty(`--${this.name}`, this.value+suffix);
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
-inputs.forEach(input=>input.addEventListener('change',handleUpdate));
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+//uppgift 3
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+const people = [
+  'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+  'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+  'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+  'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+  'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+];
